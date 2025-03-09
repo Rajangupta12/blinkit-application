1d392bfa5189f4aabbdaee3967e381b50983f34e
KARTIK KUMAR <kartikkumarji24@gmail.com>
2025-03-09 17:59:35 +0530

    difine the file structure

--- Login page/login.js
+++ Login page/login.js
@@ -19,7 +19,7 @@ document.getElementById("registerForm").addEventListener("submit", async functio
     }
 
     try {
-        const response = await fetch("http://localhost:8081/api/v1/user/register", {
+        const response = await fetch("http://localhost:8081/api/v1/auth/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ name, email, password })
